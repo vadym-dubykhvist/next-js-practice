bdd3faa5245f7310aea1038392fb9303021852a5
vadym-dubykhvist <79746411+vadym-dubykhvist@users.noreply.github.com>
2026-05-03 15:03:28 +0200

    Initial project setup with components and assets

---
+++ lib/constants.ts
@@ -0,0 +1,50 @@
+export const events = [
+  {
+    image: "/images/event1.png",
+    title: "React Summit 2026",
+    slug: "react-summit-2026",
+    location: "Amsterdam, Netherlands",
+    date: "June 16-17",
+    time: "9:00 AM - 6:00 PM"
+  },
+  {
+    image: "/images/event2.png",
+    title: "NextJS Conf 2026",
+    slug: "nextjs-conf-2026",
+    location: "San Francisco, USA",
+    date: "May 28-29",
+    time: "8:00 AM - 5:00 PM"
+  },
+  {
+    image: "/images/event3.png",
+    title: "TypeScript Meetup",
+    slug: "typescript-meetup-sf",
+    location: "San Francisco, USA",
+    date: "May 15",
+    time: "6:00 PM - 8:30 PM"
+  },
+  {
+    image: "/images/event4.png",
+    title: "DevOps Days NYC",
+    slug: "devops-days-nyc",
+    location: "New York, USA",
+    date: "June 5-6",
+    time: "8:30 AM - 5:30 PM"
+  },
+  {
+    image: "/images/event5.png",
+    title: "Web Dev Hackathon",
+    slug: "web-dev-hackathon-london",
+    location: "London, UK",
+    date: "June 22-23",
+    time: "10:00 AM - 10:00 PM"
+  },
+  {
+    image: "/images/event6.png",
+    title: "JavaScript Roundup 2026",
+    slug: "javascript-roundup-2026",
+    location: "Tokyo, Japan",
+    date: "July 10-11",
+    time: "9:00 AM - 6:00 PM"
+  }
+];
